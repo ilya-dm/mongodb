97e9792ca3f51de46c0ebd37df44ff06944cf03b
ilya-dm <ilyadmitren@mail.ru>
2020-05-01 16:39:23 +0700

    исправление замечаний

--- main.py
+++ main.py
@@ -4,42 +4,28 @@ import re
 from pymongo import MongoClient
 
 
-def read_data(db,csv_file):
-    shedule = list()
+def read_data(db, csv_file):
+    schedule = list()
 
     with open(csv_file, encoding='utf8') as csvfile:
         # прочитать файл с данными и записать в коллекцию
-        reader = csv.DictReader(csvfile,  delimiter=',')
+        reader = csv.DictReader(csvfile, delimiter=',')
         for line in reader:
-            line['Цена']= int(line['Цена'])
-            shedule.append(line)
-    result = concerts_collection.insert_many(shedule)
+            line['Цена'] = int(line['Цена'])
+            schedule.append(line)
+    result = concerts_collection.insert_many(schedule)
     return result
 
 
-
 def find_cheapest(db):
-
-    for concert in concerts_collection.find():
-        concert["Цена"]= int(concert["Цена"])
     result = concerts_collection.find().sort("Цена", direction=1).limit(1)
     return list(result)
 
 
 def find_by_name(name, db):
-    """
-    Найти билеты по имени исполнителя (в том числе – по подстроке, например "Seconds to"),
-    и вернуть их по возрастанию цены
-    """
-    search_result = list()
-    for concert in db.find().sort("Цена", direction=1):
-
-        search = (re.search(name, concert["Исполнитель"]))
-        if search:
-            search_result.append(concert)
-    return search_result
-    # result = db.find({"Исполнитель": name}).sort("Цена", direction=1)
-    # return result
+    regex = re.compile(name, re.IGNORECASE)
+    result = list(db.find({"Исполнитель": regex}).sort("Цена", direction=1))
+    return result
 
 
 if __name__ == '__main__':
@@ -47,5 +33,4 @@ if __name__ == '__main__':
     concerts_db = client['admin']
     concerts_collection = concerts_db['concerts']
     #read_data(concerts_db, 'artists.csv')
-    print(find_by_name("Джа", concerts_collection))
-
+    print(find_by_name("jon", concerts_collection))
